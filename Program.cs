90c4118167e53d8c27b03d93a7500d9cb347eaef
bhumika1111 <bhoyarpatil@434>
2022-07-29 22:57:53 +0530

    Greeting Message

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace LineComparision
+{
+    class Program
+    {
+        public static void  Main(string[] args)
+        {
+            Console.WriteLine(" **:::::::Welcome To Line Comparision Computation Program:::::::::::::*** ");
+
+            Console.WriteLine("**Output For Length Of Line**");
+            Length.CheckLengthOfLine();
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Output For Two Lines Are Equal Based On The End Points");
+            EqualityOfLines.CheckEqualityOfTwoLines();
+            Console.WriteLine("::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::::");
+            Console.WriteLine("**Output For One Lines Is Equal ,Greater Or Less Than Equal To Other Line**");
+            CompareTheLine.CheckComparisionOfLines();
+
+
+        }
+    }
+}
